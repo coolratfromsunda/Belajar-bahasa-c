404852c88b0a8738b4f0a030c955f2f700ad17a9
coolratfromsunda <105347307+coolratfromsunda@users.noreply.github.com>
2022-07-09 08:35:30 +0700

    Add files via upload

---
+++ Operator Aritmatika.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+    int e = 20;
+    int r = 2;
+    
+    printf("Hasil = \n%d", e*r);
+    printf("Hasil = \n%d", e:r);
+    printf("Hasil = \n%d", e+r);
+    printf("Hasil = \n%d", e-r);
+
+} 
