5d5e6fe2b9c3990ca4356d144588a66e0ba9a3f3
coolratfromsunda <105347307+coolratfromsunda@users.noreply.github.com>
2022-07-17 21:12:28 +0700

    Update Operator Logika.c

--- Operator Logika.c
+++ Operator Logika.c
@@ -4,16 +4,16 @@ void main() {
     int a = 5, b = 5, c = 10, result;
 
     result = (a == b) && (c > b);
-    printf("(a == b) && (c > b) adalah \n %d", result);
+    printf("(a == b) && (c > b) adalah %d\n", result);
 
     result = (a == b) && (c < b);
-    printf("(a == b) && (c < b) adalah \n %d", result);
+    printf("(a == b) && (c < b) adalah %d\n", result);
 
     result = (a == b) || (c < b);
-    printf("(a == b) || (c < b) adalah \n %d", result);
+    printf("(a == b) || (c < b) adalah %d\n", result);
 
     result = (a != b) || (c < b);
-    printf("(a != b) || (c < b) adalah \n %d", result);
+    printf("(a != b) || (c < b) adalah %d\n", result);
 
     return 0;
 }
