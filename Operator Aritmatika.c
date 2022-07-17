18d3858482df8e3b37046e0c49fb89b408ede867
coolratfromsunda <105347307+coolratfromsunda@users.noreply.github.com>
2022-07-17 20:59:26 +0700

    Update Operator Aritmatika.c

--- Operator Aritmatika.c
+++ Operator Aritmatika.c
@@ -19,8 +19,16 @@ int main()
 
     hasil = a*b;
     printf("a * b =%2.lf\n", hasil);
-    
+
     hasil = pow(5,55);
     printf("5 ^ 55 =%2.lf\n",hasil);
+
+    printf("%d\n", 4<5);
+     printf("%d\n", 4>5);
+      printf("%d\n", 4*5);
+       printf("%d\n", 4/5);
+        printf("%d\n", 4-5);
+         printf("%d\n", 4+5);
+          printf("%d", 4%5);
     return 0;
 }
